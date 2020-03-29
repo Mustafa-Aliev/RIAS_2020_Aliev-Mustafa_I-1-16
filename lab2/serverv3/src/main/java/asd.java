2b1e91e2b1716e4bb49840129d072502ca3524a7
Ibraim <adjibekirov.i1.16@gmail.com>
2020-03-29 19:11:26 +0300

    RIAS LABS

---
+++ lab2/serverv3/src/main/java/asd.java
@@ -0,0 +1,9 @@
+public class asd {
+    public static void main(String[] args) {
+        String h = "hello";
+        for(int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+    }
+}
+
